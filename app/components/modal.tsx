83f71480bbe1deaa55aab574ab93e17901a78d59
Tsiory <hei.tsiory@gmail.com>
2024-05-30 23:13:00 +0300

    appointment inforamtion

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,41 +1,28 @@
+import AppointmentForm from "./buttonClick/appointmentForm";
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button
-    
-  } from '@chakra-ui/react'
-interface modalProp{
-    content : React.ReactNode,
-    open : boolean
-    close : ()=>void
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+interface modalProp {
+  content: React.ReactNode;
+  open: boolean;
+  close: () => void;
 }
-const  ModalCompenent: React.FC<modalProp>= ({content,open,close})=> {
+const ModalCompenent: React.FC<modalProp> = ({ content, open, close }) => {
   return (
     <div>
-       <Modal isOpen={open} onClose={close}>
+      <Modal isOpen={open} onClose={close} >
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            {content}
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={close}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
+        <AppointmentForm/>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default ModalCompenent
+export default ModalCompenent;
