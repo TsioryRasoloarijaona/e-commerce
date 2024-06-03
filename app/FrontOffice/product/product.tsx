a2b076f0c8dc7fb9e14f9fcec382b0c4072f4659
Tsiory <hei.tsiory@gmail.com>
2024-06-03 19:03:36 +0300

    all product list

--- app/FrontOffice/product/product.tsx
+++ app/FrontOffice/product/product.tsx
@@ -2,21 +2,23 @@ import Card from "../../components/card";
 import { getData } from "@/app/hooks/getData";
 import { carInterface } from "@/app/interface/carInterface";
 export default async function Product() {
-  const car : carInterface[] = await getData("http://localhost:8080/car/allCar")
+  const car: carInterface[] = await getData("http://localhost:8080/car/pined");
+
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center mx-auto bg-gray-950 pt-7">
-      
-     {car.map((el)=>(
-        <Card
-        carName={el.name}
-        description={el.description}
-        imgURL={el.images[0]}
-        detailLink="/FrontOffice/product/list/"
-        price={el.price}
-        id={el.id}
-        key={el.id}
-      />
-     ))} 
+      {car.map((el) => (
+        <div>
+          <Card
+            carName={el.name}
+            description={el.description}
+            imgURL={el.images[0]}
+            detailLink="/FrontOffice/product/list/details"
+            price={el.price}
+            id={el.id}
+            key={el.id}
+          />
+        </div>
+      ))}
     </div>
   );
 }
