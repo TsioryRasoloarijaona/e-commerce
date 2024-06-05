d2d5633c81ec8889dc84971b66a34d703f9b22e3
Tsiory <hei.tsiory@gmail.com>
2024-06-05 10:23:01 +0300

    chore: appointment interface

---
+++ app/interface/appointmentInterface.ts
@@ -0,0 +1,15 @@
+interface Car {
+    id: number;
+  }
+  
+export interface appointmentRequest {
+    car: Car;
+    firstName: string;
+    email: string;
+    message: string;
+    contact: string;
+    appointmentDate: string;
+  }
+  
+
+  
