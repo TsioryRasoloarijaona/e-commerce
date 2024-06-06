6b75a8d71d55c41909da8f87ca2ecfc2f12b198a
Tsiory <hei.tsiory@gmail.com>
2024-06-06 14:29:02 +0300

    modif: style

--- app/FrontOffice/product/list/filter/brandFilter.tsx
+++ app/FrontOffice/product/list/filter/brandFilter.tsx
@@ -15,7 +15,7 @@ export default function BrandFilter({list} : {list: string[]}) {
     <div className="bg-transparent space-x-3">
 
       {list.map((el , index)=>
-      <button  className={`${selectedButton === index ? 'bg-white text-black' : 'bg-transparent text-white'}  px-4 py-2 border border-gray-700 rounded-full`}
+      <button  className={`${selectedButton === index ? 'bg-white text-black' : 'bg-transparent text-white'}  px-4 py-2 border border-gray-700 rounded-full mb-2`}
       onClick={()=>handleClick(index , el)}>
         {el}
       </button>
