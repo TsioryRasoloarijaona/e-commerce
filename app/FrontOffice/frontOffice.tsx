74583d54e1007369ab2aebd9b9c5d84bcb3b0f56
HarivolaANDY <hei.andy.43@gmail.com>
2024-06-07 20:29:00 +0300

    chore: add footer

--- app/FrontOffice/frontOffice.tsx
+++ app/FrontOffice/frontOffice.tsx
@@ -1,11 +1,13 @@
 import Hero from "./hero/hero"
 import Product from "./product/product"
+import Footer from "../components/footer"
 
 export default function FrontOffice() {
   return (
     <div>
       <Hero/>
       <Product/>
+      <Footer/>
     </div>
   )
 }
