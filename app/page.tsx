ddd02c5e1fa926518f19aa739629eaf912351e1a
HarivolaANDY <hei.andy.43@gmail.com>
2024-06-14 12:01:17 +0300

    chore: delete lines

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import FrontOffice from "./FrontOffice/frontOffice";
-import Product from "./FrontOffice/product/product";
+
 function Home() {
-
-
   return (
       <div>
         <FrontOffice/>
-        
       </div>
   );
 }
